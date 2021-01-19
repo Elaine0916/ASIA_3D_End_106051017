76c22ea5e198e61c78adade1e06848de929a3cb0
Elaine0916 <55180609+Elaine0916@users.noreply.github.com>
2021-01-19 12:02:15 +0800

    NPC對話

---
+++ 3D_End_106051017/Assets/Script/NPCDeta.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu(fileName ="NPC 資料", menuName ="Elaine/NPC 資料")]
+public class NPCDeta : ScriptableObject
+{
+    [Header("第一段對話")]
+    public string dialougA;
+    [Header("第二段對話")]
+    public string dialougB;
+}
